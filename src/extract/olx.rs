7b5a5e736f73fca3ecf497f01f8e3c27c7f557bc
Sebastian Sipos <sebastiansipos90@gmail.com>
2022-10-28 13:18:13 +0300

    olx extractor

--- src/extract/olx.rs
+++ src/extract/olx.rs
@@ -1,8 +1,12 @@
+use anyhow::{anyhow, Context};
 use chrono::{DateTime, Utc};
 
 use crate::util::Currency;
 
-use super::extractor::SavedPage;
+use super::{
+    classified::{Classified, PropertyType, SellerType},
+    extractor::SavedPage,
+};
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -11,7 +15,7 @@ struct OlxClassifiedLocation {
     region_name: String,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct OlxClassifiedParam {
     key: String,
@@ -32,7 +36,7 @@ struct OlxClassifiedUser {
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct OlxClassifiedRegularPrice {
-    value: f32,
+    value: f64,
     currency_code: Currency,
     negotiable: bool,
 }
@@ -78,7 +82,8 @@ struct OlxClassifiedWrapper {
 const JS_JSON_LINE_PREFIX: &str = "        window.__PRERENDERED_STATE__= ";
 
 pub fn extract_page_json(page: &SavedPage) -> anyhow::Result<String> {
-    let selector = scraper::Selector::parse("script#olx-init-config").unwrap();
+    let selector = scraper::Selector::parse("script#olx-init-config")
+        .map_err(|e| anyhow!("Failed to parse selector {:?}", e))?;
     let document = scraper::Html::parse_document(&page.content);
 
     let el = document
@@ -108,9 +113,55 @@ pub fn extract_page_json(page: &SavedPage) -> anyhow::Result<String> {
         .ok_or_else(|| anyhow::anyhow!("Failed to unescape Javascript JSON string."))
 }
 
+pub fn parse_classified<'a, 'b>(
+    session: &'a uuid::Uuid,
+    page: &'b SavedPage,
+) -> anyhow::Result<Classified<'a, 'b>> {
+    let json = extract_page_json(page).context("Failed extracting OLX JSON from page.")?;
+
+    let olx_classified_wrapper: OlxClassifiedWrapper =
+        serde_json::from_str(json.as_str()).context("Failed parsing OLX JSON.")?;
+
+    let o = olx_classified_wrapper.ad.ad;
+
+    Ok(Classified {
+        session,
+        url: &page.url,
+        face: o.params.iter().find_map(|_| None),
+        floor: o
+            .params
+            .iter()
+            .find_map(|p| match p.key == "floor" {
+                true => Some(p.value.as_str()),
+                _ => None,
+            })
+            .map_or(Ok(None), |v| match v {
+                "parter" | "Parter" => Ok(Some(0)),
+                v => v.parse().map(Some).context("Failed parsing OLX floor"),
+            })?,
+        layout: o.params.iter().find_map(|_| None),
+        negotiable: o.price.regular_price.negotiable,
+        price: o.price.regular_price.value,
+        property_type: PropertyType::find_in_str(o.description.as_str())
+            .unwrap_or(PropertyType::Apartment),
+        published_at: o.created_time,
+        room_count: None, // TODO:
+        seller_name: o.user.name,
+        seller_type: match o.user.company_name.is_empty() {
+            true => SellerType::Private,
+            _ => SellerType::Company,
+        },
+        surface: None,
+        title: o.title,
+        year: None,
+    })
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{super::extractor::SavedPage, extract_page_json, OlxClassifiedWrapper};
+    use super::{
+        super::extractor::SavedPage, extract_page_json, parse_classified, OlxClassifiedWrapper,
+    };
     use crate::page::PageType;
 
     #[test]
@@ -127,5 +178,22 @@ mod tests {
         let json = extract_page_json(&page).unwrap();
 
         let _olx_classified: OlxClassifiedWrapper = serde_json::from_str(json.as_str()).unwrap();
+    }
+    #[test]
+    fn smf() {
+        let session = uuid::Uuid::new_v4();
+        let page = SavedPage {
+            url: "https://www.olx.ro/d/oferta/garsoniera-uzina-2-IDgC0Kq.html".into(),
+            content: String::from_utf8(
+                std::fs::read("src/extract/test_assets/extract_olx_works.html").unwrap(),
+            )
+            .unwrap(),
+            page_type: PageType::OlxItem,
+            crawled_at: chrono::offset::Utc::now(),
+        };
+
+        if let Err(e) = parse_classified(&session, &page) {
+            panic!("{:?}", e);
+        }
     }
 }
